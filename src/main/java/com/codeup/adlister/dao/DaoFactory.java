40cc6936ca7d998b56eb1924008a809d46a4f2b9
Noland Seigler <nolandseigler@gmail.com>
2020-03-09 16:52:03 -0500

    daofactory updated to added reviews dao

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.dao;
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
+    private static Reviews reviewssDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -17,5 +18,11 @@ public class DaoFactory {
             usersDao = new MySQLUsersDao(config);
         }
         return usersDao;
+    }
+    public static Reviews getReviewsDao() {
+        if (usersDao == null) {
+            reviewsDao = new MySQLUsersDao(config);
+        }
+        return reviewsDao;
     }
 }
